79037884619148c5bec7cea7dd6b3dcd08c26dbc
Gorkem Gocer <gorkemgocer7@gmail.com>
2023-02-13 22:57:21 +0300

    ready to test deploy

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -11,13 +11,16 @@ const verifyAdmin = (req, res, next) => {
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) return res.status(403).json({ message: "Forbidden" });
-    if (decoded.userInfo.role !== "admin") {
+
+    if (decoded.UserInfo.role !== "admin") {
       return res.status(403).json({ message: "You need to loggin as Admin!" });
     }
+
     req._id = decoded.UserInfo._id;
     req.email = decoded.UserInfo.email;
     req.user = decoded.UserInfo.username;
     req.role = decoded.UserInfo.role;
+
     next();
   });
 };
