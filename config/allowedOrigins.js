835a52a7acd85074dc4364c7927a981edb72b79d
Gorkem Gocer <gorkemgocer7@gmail.com>
2023-07-06 10:20:08 +0300

    cors issue

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,5 +2,6 @@ const allowedOrigins = [
   "http://study-abroad-frontend.vercel.app",
   "http://study-abroad-frontend-git-main-kreatorxox.vercel.app",
   "http://study-abroad-frontend-94h0evcfi-kreatorxox.vercel.app",
+  ,
 ];
 module.exports = allowedOrigins;
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,14 +1,15 @@
 const allowedOrigins = require("./allowedOrigins");
 
 const corsOpts = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Cors origin not allowed"));
-    }
-  },
+  // origin: function (origin, callback) {
+  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error("Cors origin not allowed"));
+  //   }
+  // },
   // origin: "*",
+  origin: true,
   credentials: true,
   optionsSuccessStatus: 200, // def:204 for smart tvs and some old browsers support
 };
